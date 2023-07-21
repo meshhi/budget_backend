4d17bd477f37097c2830ef848a75b7fe6c6798b8
meshi <mehhi@yandex.ru>
2023-07-21 22:37:30 +0300

    f

--- initdb.sh
+++ initdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name pg -p 3003:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=backend_db -d postgres:latest
+docker run --name pg -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=1q2w341q2w34 -e POSTGRES_DB=budget -d postgres:latest
